ffde0cb03500442dfe681b266e827b784911ac0e
sweety <smehta1289@gmail.com>
2013-11-13 01:22:16 +0530

    cron job

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-sm-rc164
-========
+Railscast sm-rc164
+==================
+cron Job
+```
+cron is used when schdule jobs are recurring
+```
+git clone
+```
+git clone https://github.com/sweetymehta/sm-rc164.git
+```
+Gem file
+```
+gem 'whenever'
+bundlle install
+```
+Go to terminal
+```
+wheneverize
+and then you will have config/schedule.rb file
+```
+Now write cron jobs there
+```
+every :sunday , :at => '4:20 am' do
+  command "rm something"
+end
+```
+Terminal to see all jobs
+```
+whenever
+```
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmRc164::Application.config.secret_token = '7fb33724d06416301510a5a364db7d0c6b15947aad91e82eca9927fd8fb3223f0bfb4014968c370f9af1fd85af891bf4a448833acb0879d67cea46b3e2960157'
